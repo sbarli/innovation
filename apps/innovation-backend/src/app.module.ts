fab3e8da4120553ed79af4ce6a91617b53445c70
Samantha Barli <samanthasalley@gmail.com>
2023-09-19 23:39:50 -0700

    feat/be/install and configure GQL in NestJS app

--- apps/innovation-backend/src/app.module.ts
+++ apps/innovation-backend/src/app.module.ts
@@ -7,6 +7,9 @@ import { GamesModule } from './games/games.module';
 import { PlayerGameDetailsModule } from './player-game-details/player-game-details.module';
 import { PlayersModule } from './players/players.module';
 import { GameplayModule } from './gameplay/gameplay.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GqlConfigService } from './graphql/gql-config-service';
 
 const MONGO_URL = process.env.MONGO_URL || '';
 console.log('MONGO_URL: ', MONGO_URL);
@@ -14,6 +17,10 @@ console.log('MONGO_URL: ', MONGO_URL);
 @Module({
   imports: [
     MongooseModule.forRoot(MONGO_URL),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useClass: GqlConfigService,
+    }),
     CardsModule,
     PlayersModule,
     GamesModule,
