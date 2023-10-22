c3bb6bfd50fd564fe6f7982bd3206a93843b5646
Samantha Barli <samanthasalley@gmail.com>
2023-10-21 17:50:01 -0700

    chore/ui/add custom eslint config & fix lint issues

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig, Options } from "tsup";
+import { defineConfig, Options } from 'tsup';
 
+// eslint-disable-next-line import/no-default-export
 export default defineConfig((options: Options) => ({
   entry: {
-    index: "src/index.tsx",
+    index: 'src/index.tsx',
   },
   banner: {
     js: "'use client'",
   },
   clean: true,
-  format: ["cjs", "esm"],
-  external: ["react"],
+  format: ['cjs', 'esm'],
+  external: ['react'],
   dts: true,
   ...options,
 }));
