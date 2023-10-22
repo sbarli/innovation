30426802f1a08ce0e8cebdafb342d6f07d6f4094
Samantha Barli <samanthasalley@gmail.com>
2023-10-21 17:49:31 -0700

    chore/lint/lint config file

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,80 +1,80 @@
 module.exports = {
-  plugins: ["@typescript-eslint/eslint-plugin", "turbo", "prettier", "import"],
+  plugins: ['@typescript-eslint/eslint-plugin', 'turbo', 'prettier', 'import'],
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "prettier",
-    "eslint-config-turbo",
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'eslint-config-turbo',
   ],
   env: {
     es6: true,
   },
   ignorePatterns: [
-    ".prettierrc",
-    ".eslintrc.js",
-    "jest.config.ts",
-    "node_modules",
-    "node_modules/**",
-    "**/node_modules",
-    "**/*.generated.*",
-    "**/*.d.ts",
-    "dist",
-    "dist/",
-    "dist/**",
-    "**/dist",
+    '.prettierrc',
+    '.eslintrc.js',
+    'jest.config.ts',
+    'node_modules',
+    'node_modules/**',
+    '**/node_modules',
+    '**/*.generated.*',
+    '**/*.d.ts',
+    'dist',
+    'dist/',
+    'dist/**',
+    '**/dist',
   ],
   rules: {
-    "import/order": [
-      "error",
+    'import/order': [
+      'error',
       {
-        "groups": ["builtin", "external", "internal", "parent", "sibling", "index"],
-        "newlines-between": "always",
-        "alphabetize": {
-          "order": "asc",
-          "caseInsensitive": false
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: false,
         },
-        "pathGroups": [
+        pathGroups: [
           {
-            "pattern": "react",
-            "position": "before",
-            "group": "external"
+            pattern: 'react',
+            position: 'before',
+            group: 'external',
           },
           {
-            "pattern": "{src}/**",
-            "group": "internal"
+            pattern: '{src}/**',
+            group: 'internal',
           },
           {
-            "pattern": "@inno/**",
-            "position": "before",
-            "group": "internal"
-          }
+            pattern: '@inno/**',
+            position: 'before',
+            group: 'internal',
+          },
         ],
-        "pathGroupsExcludedImportTypes": [],
-      }
+        pathGroupsExcludedImportTypes: [],
+      },
     ],
-    "import/no-default-export": "error",
-    "comma-dangle": [
-      "error",
+    'import/no-default-export': 'error',
+    'comma-dangle': [
+      'error',
       {
-        "arrays": "always-multiline",
-        "objects": "always-multiline",
-        "imports": "always-multiline",
-        "exports": "always-multiline",
-        "functions": "never",
-      }
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
     ],
-    "prettier/prettier": [
+    'prettier/prettier': [
       2,
       {
-        "arrowParens": "always",
-        "semi": true,
-        "singleQuote": true,
-        "trailingComma": "es5",
-        printWidth: 100
+        arrowParens: 'always',
+        semi: true,
+        singleQuote: true,
+        trailingComma: 'es5',
+        printWidth: 100,
       },
       {
-        "usePrettierrc": false
-      }
-    ]
-  }
+        usePrettierrc: false,
+      },
+    ],
+  },
 };
