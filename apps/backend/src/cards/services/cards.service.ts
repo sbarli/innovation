056e6f3416876079eb732dc29ab9f7569aae7154
Samantha Barli <samanthasalley@gmail.com>
2023-10-25 22:15:56 -0700

    feat/be/add color to Cards data

--- apps/backend/src/cards/services/cards.service.ts
+++ apps/backend/src/cards/services/cards.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
-import { CreateCardDto } from '../dto/create-card.dto';
+import { getCatchErrorMessage } from '@inno/utils';
+
+import { transformRawCardsToCardSchema } from '../helpers/transform-raw-cards-to-card-schema';
 import { Card, CardDocument } from '../schemas/card.schema';
 
 @Injectable()
@@ -29,17 +31,36 @@ export class CardsService {
     return this.cardModel.findOne({ cardId });
   }
 
-  async create(createCardDto: CreateCardDto): Promise<Card> {
+  async create(createCardDto: Omit<Card, '_id'>): Promise<Card> {
     const createdCard = new this.cardModel(createCardDto);
     return createdCard.save();
   }
 
-  // TODO: optimize this
-  async createMany(cardsToCreate: CreateCardDto[]): Promise<Card[]> {
-    return Promise.all(cardsToCreate.map((cardDto) => this.create(cardDto)));
+  async createMany(cardsToCreate: Omit<Card, '_id'>[]): Promise<Card[]> {
+    return this.cardModel.insertMany(cardsToCreate);
   }
 
   async deleteAll() {
     return this.cardModel.deleteMany({});
+  }
+
+  async seedCards(): Promise<Card[]> {
+    const session = await this.cardModel.startSession();
+    try {
+      const cardsToSeed = transformRawCardsToCardSchema();
+      session.startTransaction();
+      await this.deleteAll();
+      const createdCards = await this.createMany(cardsToSeed);
+      await session.commitTransaction();
+      return createdCards;
+    } catch (error) {
+      await session.abortTransaction();
+      throw new Error(
+        getCatchErrorMessage(error) ??
+          'cardsService.seedCards: Could not seed cards. Transaction aborted.'
+      );
+    } finally {
+      await session.endSession();
+    }
   }
 }
