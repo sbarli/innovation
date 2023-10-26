87a2526c142c80a6fcf887ca7e66e63164552806
Samantha Barli (Salley) <samanthasalley@gmail.com>
2023-10-25 18:30:43 -0700

    Test/be/resources service unit tests (#33)
    * chore/be/update mocks for testing ResourcesService
    
    * test/be/add unit tests for ResourcesService

--- apps/backend/src/cards/__mocks__/resource-totals.mock.ts
+++ apps/backend/src/cards/__mocks__/resource-totals.mock.ts
@@ -8,3 +8,12 @@ export const MOCK_RESOURCE_TOTALS: ResourceTotals = {
   factories: 0,
   timepieces: 0,
 };
+
+export const MOCK_DOUBLE_RESOURCE_TOTALS: ResourceTotals = {
+  castles: 0,
+  crowns: 2,
+  leaves: 0,
+  lightbulbs: 4,
+  factories: 0,
+  timepieces: 0,
+};
