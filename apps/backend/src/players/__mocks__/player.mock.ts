93cd5cc6e75822b158bf5ba12cce46c158c15d64
Samantha Barli (Salley) <samanthasalley@gmail.com>
2023-10-30 21:40:07 -0700

    Feat/be/new create players mutation (#52)
    * feat/be/new createPlayers mutation and service method
    update/be/update playersService.create input shape
    
    * chore/be/regen gql schema with new createPlayers mutation & input type
    
    * test/be/new createPlayers mutation tests + update player mocks
    
    * fix/be/update CreatePlayersInput.names field type & regen schema

--- apps/backend/src/players/__mocks__/player.mock.ts
+++ apps/backend/src/players/__mocks__/player.mock.ts
@@ -1,4 +1,5 @@
 import { CreatePlayerInput } from '../dto/create-player.dto';
+import { CreatePlayersInput } from '../dto/create-players.dto';
 import { GetPlayerDto } from '../dto/get-player.dto';
 import { GetPlayersInput } from '../dto/get-players.dto';
 import { GetPlayerFieldOptions } from '../players.types';
@@ -6,10 +7,18 @@ import { Player } from '../schemas/player.schema';
 
 export const MOCK_ID = 'mock_id';
 export const MOCK_PLAYER_ID = 'mockplayer';
+export const MOCK_PLAYER_ID_2 = 'mockplayer2';
 export const MOCK_PLAYER_NAME = 'Mock Player';
+export const MOCK_PLAYER_NAME_2 = 'Mock Player 2';
+export const MOCK_PLAYER_REF_1 = 'mock-player-ref-1';
+export const MOCK_PLAYER_REF_2 = 'mock-player-ref-2';
 
 export const MOCK_PLAYER_INPUT: CreatePlayerInput = {
   name: MOCK_PLAYER_NAME,
+};
+
+export const MOCK_PLAYERS_INPUT: CreatePlayersInput = {
+  names: [MOCK_PLAYER_NAME, MOCK_PLAYER_NAME_2],
 };
 
 export const MOCK_PLAYER: Player = { _id: MOCK_ID, playerId: MOCK_PLAYER_ID, ...MOCK_PLAYER_INPUT };
@@ -44,9 +53,9 @@ export const MOCK_GET_PLAYERS_BY_INVALID_INPUT: GetPlayersInput = {
   searchValues: [MOCK_ID],
 };
 
-export const MOCK_PLAYER_REF_1 = 'mock-player-ref-1';
-export const MOCK_PLAYER_REF_2 = 'mock-player-ref-2';
-
 export const MOCK_PLAYER_1 = Object.assign({}, { ...MOCK_PLAYER, _id: MOCK_PLAYER_REF_1 });
-export const MOCK_PLAYER_2 = Object.assign({}, { ...MOCK_PLAYER, _id: MOCK_PLAYER_REF_2 });
+export const MOCK_PLAYER_2 = Object.assign(
+  {},
+  { ...MOCK_PLAYER, _id: MOCK_PLAYER_REF_2, playerId: MOCK_PLAYER_ID_2, name: MOCK_PLAYER_NAME_2 }
+);
 export const MOCK_PLAYERS = [MOCK_PLAYER_1, MOCK_PLAYER_2];
