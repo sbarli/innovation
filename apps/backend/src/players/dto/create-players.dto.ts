af6ddb1a775521dda088a041729b7522d0c83f74
Samantha Barli <samanthasalley@gmail.com>
2023-10-30 21:36:59 -0700

    fix/be/update CreatePlayersInput.names field type & regen schema

--- apps/backend/src/players/dto/create-players.dto.ts
+++ apps/backend/src/players/dto/create-players.dto.ts
@@ -3,6 +3,6 @@ import { Field, InputType, ObjectType } from '@nestjs/graphql';
 @ObjectType()
 @InputType()
 export class CreatePlayersInput {
-  @Field(() => String)
+  @Field(() => [String])
   names!: string[];
 }
