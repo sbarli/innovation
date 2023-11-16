4c08b1809e47d2fef2b314e702599778dfce3563
Samantha Barli <samanthasalley@gmail.com>
2023-11-15 23:18:03 -0800

    feat/be/globally configure CacheManager

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
@@ -9,9 +10,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CardsModule } from './cards/cards.module';
+import { DEFAULT_CACHE_TTL } from './constants/cache.constants';
 import { GameplayModule } from './gameplay/gameplay.module';
 import { GamesModule } from './games/games.module';
-import { GqlConfigService } from './graphql/gql-config-service';
+import { GqlConfigService } from './graphql/gql-config.service';
 import { PlayerGameDetailsModule } from './player-game-details/player-game-details.module';
 import { PlayersModule } from './players/players.module';
 import { SocketModule } from './socket/socket.module';
@@ -33,6 +35,10 @@ import { UsersModule } from './users/users.module';
     GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
       useClass: GqlConfigService,
+    }),
+    CacheModule.register({
+      isGlobal: true,
+      ttl: DEFAULT_CACHE_TTL,
     }),
     AuthModule,
     UsersModule,
