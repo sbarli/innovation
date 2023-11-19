d0b95af0f7f011ecfa50c655e0fb4d69d525d4f8
Samantha Barli <samanthasalley@gmail.com>
2023-11-19 13:54:49 -0800

    feat/rn/set bearer token Auth header on GQL provider, if exists

--- apps/native/src/graphql/client.ts
+++ apps/native/src/graphql/client.ts
@@ -1,8 +1,25 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import { BACKEND_GQL_API } from '../app-core/constants/manifest';
+import { StorageKeys } from '../storage/storage.constants';
+import { getAsyncStorageItem } from '../storage/useAsyncStorage';
+
+const httpLink = createHttpLink({
+  uri: BACKEND_GQL_API,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = getAsyncStorageItem(StorageKeys.AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 export const client = new ApolloClient({
-  uri: BACKEND_GQL_API,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
