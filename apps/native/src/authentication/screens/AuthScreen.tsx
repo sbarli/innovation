da8f4cb688ce54734f3a97a333c74846834a853b
Samantha Barli <samanthasalley@gmail.com>
2023-11-19 13:54:08 -0800

    feat/rn/add basic Auth components & navigation btwn login & signup form componets

---
+++ apps/native/src/authentication/screens/AuthScreen.tsx
@@ -0,0 +1,34 @@
+import { Box, Heading, Text } from '@gluestack-ui/themed';
+import { useLocalSearchParams } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+
+import { useIsAuthenticatedQuery } from '@inno/gql';
+
+import { AuthRouteParams } from '../../app-core/constants/navigation';
+import { LoginForm } from '../components/LoginForm';
+import { SignupForm } from '../components/SignupForm';
+
+export const AuthScreen = () => {
+  const params = useLocalSearchParams<{ type?: AuthRouteParams }>();
+  const { data, loading } = useIsAuthenticatedQuery();
+  if (loading) {
+    return (
+      <Box>
+        <Text>Checking auth status...</Text>
+      </Box>
+    );
+  }
+
+  if (!data?.isAuthenticated._id) {
+    return <Box>{params.type === AuthRouteParams.SIGNUP ? <SignupForm /> : <LoginForm />}</Box>;
+  }
+
+  return (
+    <>
+      <StatusBar style="auto" />
+      <Box alignItems="center">
+        <Heading size="lg">User Is Authenticated</Heading>
+      </Box>
+    </>
+  );
+};
