0cd44636c69d7be6acf334d267b2418a2063d0c1
Samantha Barli <samanthasalley@gmail.com>
2023-11-19 23:12:22 -0800

    fix/rn/update GQL client to use async storage correctly for getting auth token header

--- apps/native/src/graphql/client.ts
+++ apps/native/src/graphql/client.ts
@@ -1,25 +1,29 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
 
 import { BACKEND_GQL_API } from '../app-core/constants/manifest';
-import { StorageKeys } from '../storage/storage.constants';
-import { getAsyncStorageItem } from '../storage/useAsyncStorage';
+import { StorageKeys } from '../app-core/constants/storage.constants';
 
 const httpLink = createHttpLink({
   uri: BACKEND_GQL_API,
 });
 
-const authLink = setContext((_, { headers }) => {
-  const token = getAsyncStorageItem(StorageKeys.AUTH_TOKEN);
+const authLink = setContext(async (_, { headers }) => {
+  const { getItem } = useAsyncStorage(StorageKeys.AUTH_TOKEN);
+  const token = await getItem();
+
   return {
+    ...headers,
     headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : null,
     },
   };
 });
 
+const link = authLink.concat(httpLink);
+
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache(),
 });
