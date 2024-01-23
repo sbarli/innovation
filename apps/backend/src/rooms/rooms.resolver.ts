08b90dbaf502f103b8fd7833e3a03c75eae07ac0
Samantha Barli <samanthasalley@gmail.com>
2024-01-22 23:40:11 -0800

    feat/be/create rooms module with service, resolver, schemas, and basic unit tests

---
+++ apps/backend/src/rooms/rooms.resolver.ts
@@ -0,0 +1,53 @@
+import { HttpException, HttpStatus, UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { JwtGqlAuthGuard } from 'src/auth/guards/jwt-gql-auth.guard';
+import { UserWithoutPassword } from 'src/users/schemas/user.schema';
+
+import { getCatchErrorMessage } from '@inno/utils';
+
+import { RoomsService } from './rooms.service';
+import { CreateRoomInput, NullishRoom, Room } from './schemas/room.schema';
+
+@Resolver('rooms')
+export class RoomsResolver {
+  constructor(private readonly roomsService: RoomsService) {}
+
+  @Query(() => Room, { nullable: true })
+  async getRoom(@Args('roomRef', { type: () => String }) roomRef: string): Promise<NullishRoom> {
+    try {
+      return this.roomsService.findRoomByRef(roomRef);
+    } catch (error) {
+      throw new HttpException(
+        getCatchErrorMessage(error, `GetRoom Query -> Could not find room with id ${roomRef}`),
+        HttpStatus.BAD_REQUEST
+      );
+    }
+  }
+
+  @Query(() => [Room], { nullable: true })
+  @UseGuards(JwtGqlAuthGuard)
+  async getRoomsForPlayer(
+    @CurrentUser() user: UserWithoutPassword
+    // @Args('playerRef', { type: () => String }) playerRef: string
+  ): Promise<Room[]> {
+    try {
+      return this.roomsService.findRoomsByPlayerRef(user._id);
+    } catch (error) {
+      throw new HttpException(
+        getCatchErrorMessage(
+          error,
+          `getRoomsForPlayer Query -> Could not find rooms for player with id ${user._id}`
+        ),
+        HttpStatus.BAD_REQUEST
+      );
+    }
+  }
+
+  @Mutation(() => Room)
+  async createNewRoom(
+    @Args('newRoomData', { type: () => CreateRoomInput }) newRoomData: CreateRoomInput
+  ): Promise<Room> {
+    return await this.roomsService.createRoom(newRoomData);
+  }
+}
---
+++ apps/backend/src/rooms/schemas/room.schema.ts
@@ -0,0 +1,51 @@
+import { Field, ID, InputType, ObjectType, OmitType } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+export type RoomDocument = HydratedDocument<Room>;
+
+@Schema({ timestamps: true })
+@ObjectType()
+export class Room {
+  @Field(() => ID)
+  _id!: string;
+
+  @Prop()
+  @Field(() => Date, { nullable: true })
+  createdAt?: Date;
+
+  @Prop()
+  @Field(() => Date, { nullable: true })
+  updatedAt?: Date;
+
+  @Prop({ required: true, type: String })
+  @Field(() => String)
+  roomName!: string;
+
+  @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  @Field(() => ID)
+  hostRef!: string;
+
+  @Prop({
+    required: true,
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'User',
+  })
+  @Field(() => [ID])
+  connectedPlayerRefs!: string[];
+
+  @Prop({ required: true, default: true, type: Boolean })
+  @Field(() => Boolean)
+  availableToJoin!: boolean;
+}
+
+@InputType()
+export class CreateRoomInput extends OmitType(
+  Room,
+  ['_id', 'createdAt', 'updatedAt', 'connectedPlayerRefs', 'availableToJoin'] as const,
+  InputType
+) {}
+
+export type NullishRoom = Room | null | undefined;
+
+export const RoomSchema = SchemaFactory.createForClass(Room);
