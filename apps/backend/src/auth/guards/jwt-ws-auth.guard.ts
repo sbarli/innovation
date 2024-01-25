700a282883803917a9764e4eb01618dc1ff37434
Samantha Barli <samanthasalley@gmail.com>
2024-01-25 12:39:20 -0800

    feat/be/update auth decorators and ws guard

--- apps/backend/src/auth/guards/jwt-ws-auth.guard.ts
+++ apps/backend/src/auth/guards/jwt-ws-auth.guard.ts
@@ -1,37 +1,37 @@
-import { ExecutionContext } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { WsException } from '@nestjs/websockets';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const getAuthHeaderFromWSHandshake = (handshake: any) => {
   // For use with Postman
   if (handshake?.headers?.authorization) {
     return {
-      headers: {
-        authorization: handshake.headers.authorization,
-      },
+      authorization: handshake.headers.authorization,
     };
   }
   // For use with RN/Web clients (per socket-io client API docs)
   if (handshake?.auth?.token) {
     return {
-      headers: {
-        authorization: `Bearer ${handshake.auth.token}`,
-      },
+      authorization: `Bearer ${handshake.auth.token}`,
     };
   }
   // Default to bad auth state
   return {
-    headers: {
-      authorization: undefined,
-    },
+    authorization: undefined,
   };
 };
 
+@Injectable()
 export class JwtWsAuthGuard extends AuthGuard('jwt') {
   getRequest(context: ExecutionContext) {
-    const ws = context.switchToWs().getClient(); // possibly `getData()` instead.
+    const ws = context.switchToWs().getClient();
     const authHeader = getAuthHeaderFromWSHandshake(ws.handshake);
-    console.log('authHeader: ', authHeader);
-    return authHeader;
+    if (!authHeader.authorization) {
+      throw new WsException('Unauthorized');
+    }
+    const ctx = context.switchToHttp().getRequest();
+    ctx.headers = { ...ctx.headers, ...authHeader };
+    return ctx;
   }
 }
