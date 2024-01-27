3ae5fe5fbc7200edd0740fc8f0b53339d7b62088
Samantha Barli <samanthasalley@gmail.com>
2024-01-26 16:47:40 -0800

    feat/rn/update RoomsScreen to use FlatList and new RoomCard component

--- apps/native/src/rooms/screens/RoomsScreen.tsx
+++ apps/native/src/rooms/screens/RoomsScreen.tsx
@@ -1,41 +1,62 @@
-import { Box, Heading, Text } from '@gluestack-ui/themed';
+import { useEffect } from 'react';
 
-import { useGetRoomsForPlayerQuery } from '@inno/gql';
+import { Box, FlatList, HStack, Heading, Text, VStack } from '@gluestack-ui/themed';
+import { router } from 'expo-router';
 
+import { SocketEvent } from '@inno/constants';
+import { Room, RoomDataFragment, useGetRoomsForPlayerQuery } from '@inno/gql';
+
+import { Routes } from '../../app-core/constants/navigation';
 import { useAuthContext } from '../../authentication/state/AuthProvider';
 import { useSocketContext } from '../../websockets/SocketProvider';
 import { CreateRoomCTA } from '../components/CreateRoomCTA';
 import { JoinRoomCTA } from '../components/JoinRoomCTA';
+import { RoomCard } from '../components/RoomCard';
 
 export const RoomsScreen = () => {
   const { user } = useAuthContext();
   const { data, loading } = useGetRoomsForPlayerQuery();
   const { socket } = useSocketContext();
 
+  useEffect(() => {
+    socket?.on(SocketEvent.JOIN_ROOM_SUCCESS, (roomData: Room) => {
+      router.push({
+        pathname: Routes.ROOM,
+        params: { roomId: roomData._id },
+      });
+    });
+    return () => {
+      socket?.removeListener(SocketEvent.JOIN_ROOM_SUCCESS);
+    };
+  }, [socket]);
+
   if ((!data && loading) || !user) {
     return <Text>Loading Rooms</Text>;
   }
 
   return (
-    <Box flexDirection="column">
-      <Box flexDirection="row" justifyContent="space-evenly">
+    <VStack space="xl">
+      <HStack px="$5" space="xl" justifyContent="flex-end">
         <CreateRoomCTA socket={socket} />
         <JoinRoomCTA socket={socket} />
-      </Box>
+      </HStack>
       {data?.getRoomsForPlayer?.length ? (
         <Box>
-          <Heading size="lg">Your Rooms</Heading>
-          <Box flexDirection="row" justifyContent="space-between">
-            {data?.getRoomsForPlayer.map((room) => (
-              <Box key={`room-${room._id}`}>
-                <Heading>{room.name}</Heading>
-                <Text>{room.availableToJoin ? 'Open' : 'Closed'}</Text>
-                {room.hostRef === user._id ? (
-                  <Text color="$green700">You are the host!</Text>
-                ) : null}
-              </Box>
-            ))}
-          </Box>
+          <Heading mx="$5" size="lg">
+            Your Rooms
+          </Heading>
+          <FlatList
+            mx="$2.5"
+            my="$5"
+            data={data.getRoomsForPlayer}
+            horizontal
+            keyExtractor={(item) => (item as RoomDataFragment)._id}
+            renderItem={({ item }) => {
+              const room = item as RoomDataFragment;
+              const userIsHost = room.hostRef === user._id;
+              return <RoomCard room={room} metadata={{ userIsHost }} socket={socket} />;
+            }}
+          />
         </Box>
       ) : (
         <Box alignItems="center">
@@ -43,9 +64,6 @@ export const RoomsScreen = () => {
           <Text>Create one now!</Text>
         </Box>
       )}
-      <Box alignItems="center">
-        <Heading size="lg">Open Rooms</Heading>
-      </Box>
-    </Box>
+    </VStack>
   );
 };
