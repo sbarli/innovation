3795e3c644802b56bf04b19fc3ff76f72c57798c
Samantha Barli <samanthasalley@gmail.com>
2024-02-06 15:28:07 -0800

    chore/constants/add new SocketEvent enum keys

--- packages/constants/src/sockets.ts
+++ packages/constants/src/sockets.ts
@@ -14,14 +14,19 @@ export class SocketEventError {
 
 export enum SocketEvent {
   // server-emitted events
+  CLOSE_ROOM_ERROR = 'closeRoomError',
+  CLOSE_ROOM_SUCCESS = 'closeRoomSuccess',
   CREATE_ROOM_ERROR = 'createRoomError',
   CREATE_ROOM_SUCCESS = 'createRoomSuccess',
+  GET_PLAYER_ROOMS_OVERVIEW_ERROR = 'getPlayerRoomsOverviewError',
   JOIN_ROOM_ERROR = 'joinRoomError',
   JOIN_ROOM_SUCCESS = 'joinRoomSuccess',
   LEAVE_ROOM_SUCCESS = 'leaveRoomSuccess',
   LEAVE_ROOM_ERROR = 'leaveRoomError',
 
   // client-Emitted Events
+  CLOSE_ROOM = 'closeRoom',
+  GET_PLAYER_ROOMS_OVERVIEW = 'getPlayerRoomsOverview',
   JOIN_ROOM = 'joinRoom',
   LEAVE_ROOM = 'leaveRoom',
 
