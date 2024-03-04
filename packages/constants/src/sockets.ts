ba7140de1ef9676d9d5788a4e18ae5ddae4173e7
Samantha Barli <samanthasalley@gmail.com>
2024-03-04 15:35:15 -0800

    feat/constants/add start game related socket event enums

--- packages/constants/src/sockets.ts
+++ packages/constants/src/sockets.ts
@@ -28,16 +28,19 @@ export enum SocketEvent {
   // server-emitted events
   CLOSE_ROOM_ERROR = 'closeRoomError',
   JOIN_ROOM_ERROR = 'joinRoomError',
+  START_GAME_ERROR = 'startGameError',
 
   // client-Emitted Events
   CLOSE_ROOM = 'closeRoom',
   GET_ROOM_METADATA = 'getRoomMetadata',
   JOIN_ROOM = 'joinRoom',
   MAP_USER_TO_SOCKET = 'mapUserToSocket',
+  START_GAME = 'startGame',
 
   // event emitted to rooms
   CLOSE_ROOM_IN_PROGRESS = 'closeRoomInProgress',
   CLOSE_ROOM_SUCCESS = 'closeRoomSuccess',
+  GAME_STARTED = 'gameStarted',
   USER_JOINED_ROOM = 'userJoinedRoom',
   USER_LEFT_ROOM = 'userLeftRoom',
 }
