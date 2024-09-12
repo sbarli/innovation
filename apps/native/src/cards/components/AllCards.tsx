9601497c7afbccc9290627c06bda170dbf6fa774
Samantha Barli <samanthasalley@gmail.com>
2024-09-12 10:34:15 -0700

    update example cards to use new CardFrontWithDetails component

--- apps/native/src/cards/components/AllCards.tsx
+++ apps/native/src/cards/components/AllCards.tsx
@@ -2,8 +2,8 @@ import { Box, HStack, Text } from '@gluestack-ui/themed';
 
 import { useGetAllCardsQuery } from '@inno/gql';
 
+import { CardFrontWithDetails } from './CardFrontWithDetails';
 import { CardBack } from './back/CardBack';
-import { CardFront } from './front/CardFront';
 
 export const AllCards = () => {
   const { data, loading } = useGetAllCardsQuery();
@@ -16,18 +16,23 @@ export const AllCards = () => {
     );
   }
   return (
-    <Box w="$full" paddingHorizontal="$10">
-      <HStack w="$full" space="md">
-        {allCards?.slice(0, 3).map((c) => <CardFront key={c.cardId} card={c} />)}
+    <Box w="$full" paddingHorizontal="$10" gap="$2.5">
+      <HStack w="$full" space="md" gap="$2.5">
+        {allCards?.slice(0, 3).map((c) => <CardFrontWithDetails key={c.cardId} card={c} />)}
       </HStack>
-      <HStack w="$full" space="md">
+      <HStack w="$full" space="md" gap="$2.5">
+        {allCards
+          ?.slice(allCards.length - 8, allCards.length - 4)
+          .map((c) => <CardFrontWithDetails key={c.cardId} card={c} />)}
+      </HStack>
+      <HStack w="$full" space="md" gap="$2.5">
         <CardBack age={1} />
         <CardBack age={2} />
         <CardBack age={3} />
         <CardBack age={4} />
         <CardBack age={5} />
       </HStack>
-      <HStack w="$full" space="md">
+      <HStack w="$full" space="md" gap="$2.5">
         <CardBack age={6} />
         <CardBack age={7} />
         <CardBack age={8} />
