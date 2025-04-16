0d59111e2a5a1e6981e1183a6f8ab4d877125ba3
Samantha Barli <samanthasalley@gmail.com>
2025-04-16 15:16:56 -0700

    fix app entry

--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -1,10 +1,1 @@
-import { Text, View } from 'react-native';
-
-// eslint-disable-next-line import/no-default-export
-export default function Home() {
-  return (
-    <View>
-      <Text>Welcome Home</Text>
-    </View>
-  );
-}
+import 'expo-router/entry';
